123ef5ccc8fa90934a0485ce111c66c0390bea8f
shuvo37 <110215540+shuvo37@users.noreply.github.com>
2024-07-13 21:50:57 +0600

    Delete rent_a_carr/logout.php

--- rent_a_carr/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-echo "<script>alert('You have been logged out');</script>";
-echo "<script>window.location.href = 'rant_a_car.php';</script>";
-exit();
-?>
